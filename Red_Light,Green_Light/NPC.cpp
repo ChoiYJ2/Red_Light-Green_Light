b70508b535872da6134dc6b1c740da85d0224a8a
YuJeong Choi <chldb1221@naver.com>
2021-12-10 21:51:07 +0900

    NPC speed is manipulateed

--- Red_Light,Green_Light/NPC.cpp
+++ Red_Light,Green_Light/NPC.cpp
@@ -1,4 +1,35 @@
 #include <SFML/Graphics.hpp>
 #include "NPC.h"
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 using namespace sf;
+
+NPC::NPC()
+{
+	t_npc.loadFromFile("images/NPC.png");
+	t_npc_death.loadFromFile("images/NPC_death.png");
+	s_npc.setTexture(t_npc);
+	s_npc.setPosition(20.0f, 350.0f - 110.0f);
+	srand((unsigned int)time(NULL));
+	speed = rand() % 3 + 2; //NPC마다 속도 랜덤하게 (2 ~ 5)
+}
+
+void NPC::update()
+{
+	Vector2f pos = s_npc.getPosition();
+	if (end == true)
+	{
+		return;
+	}
+	if (pos.x >= 1315.0f) // 결승선 넘어가면 움직이지 않음
+	{
+		end = true;
+	}
+	s_npc.move(speed, 0.0f);
+}
+
+void NPC::draw(RenderWindow& _window)
+{
+	_window.draw(s_npc);
+}
--- Red_Light,Green_Light/NPC.h
+++ Red_Light,Green_Light/NPC.h
@@ -4,8 +4,16 @@ using namespace std;
 using namespace sf;
 class NPC
 {
+	Texture t_npc, t_npc_death;
+	Sprite s_npc;
+	float speed;
 public:
+	bool start = false;
+	bool end = false;
 	NPC();
+	void is_start();
+	void is_end();
 	void draw(RenderWindow&); //윈도우 창에 NPC그릴 때 사용
+	void update();
 	FloatRect NPCarea(); //NPC가 총에 맞으면 죽는 모션
 };
